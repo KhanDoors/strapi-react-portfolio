0877c97ed57c57731a67cb9a543f18829d28ad39
Syyad Khan <syykhan@deloitte.com>
2020-06-05 11:03:46 -0400

    get ports done

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Portfolio = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
+  const getItems = async () => {
+    await axios
+      .get("http://localhost:1337/portfolios")
+      .then((res) => setItems(res.data))
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
+
   return (
     <div>
-      <h1>Portfolio</h1>
+      <h1>Portfolios</h1>
+      {items.map((item) => (
+        <div key={item.id} style={{ backgroundColor: item.background }}>
+          <div>{item.title}</div>
+          <p>{item.description}</p>
+        </div>
+      ))}
     </div>
   );
 };
