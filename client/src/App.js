67073c67fa57c2d67d12ee40b8c189123691aca3
Syyad Khan <syykhan@deloitte.com>
2020-06-05 11:57:50 -0400

    GBTW

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Portfolio from "./components/Portfolio";
 function App() {
   return (
     <div className="App">
-      <h1>No Peace</h1>
       <Portfolio />
     </div>
   );
--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -1,7 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+    height: "100vh",
+    backgroundColor: "lightblue",
+  },
+});
 
 const Portfolio = () => {
+  const classes = useStyles();
   const [items, setItems] = useState([]);
 
   useEffect(() => {
@@ -10,7 +23,7 @@ const Portfolio = () => {
 
   const getItems = async () => {
     await axios
-      .get("http://localhost:1337/portfolios")
+      .get("http://localhost:1337/portfolios?user=1")
       .then((res) => setItems(res.data))
       .catch((err) => {
         console.log(err.response);
@@ -18,14 +31,39 @@ const Portfolio = () => {
   };
 
   return (
-    <div>
-      <h1>Portfolios</h1>
-      {items.map((item) => (
-        <div key={item.id} style={{ backgroundColor: item.background }}>
-          <div>{item.title}</div>
-          <p>{item.description}</p>
-        </div>
-      ))}
+    <div className={classes.root}>
+      <Typography variant="h1" gutterBottom>
+        Portfolios
+      </Typography>
+      <Grid container>
+        <Grid item xs={12}>
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+            spacing={2}
+          >
+            {items.map((item) => (
+              <Grid item key={item.id}>
+                <Card
+                  style={{
+                    backgroundColor: item.background,
+                    width: "40em",
+                  }}
+                >
+                  <Typography variant="h3" gutterBottom>
+                    {item.title}
+                  </Typography>
+                  <Typography variant="h5" gutterBottom>
+                    {item.description}
+                  </Typography>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 };
