66b8167ce7e5b4f4ef7d2f6ea9ba473e4cfa5617
Syyad Khan <syykhan@deloitte.com>
2020-06-05 09:40:08 -0400

    react setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-
-import "./App.css";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
     <div className="App">
       <h1>No Peace</h1>
+      <Portfolio />
     </div>
   );
 }
