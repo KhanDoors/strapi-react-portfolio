969d8ec03bb195348bd6fffabc80f3268bc5096d
Syyad Khan <syykhan@deloitte.com>
2020-06-11 19:25:25 -0400

    good night

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -4,11 +4,64 @@ import Card from "@material-ui/core/Card";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import theme from "./../theme";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import { Avatar } from "@material-ui/core";
+import BookmarksIcon from "@material-ui/icons/Bookmarks";
 
 const useStyles = makeStyles({
   root: {
     width: "100%",
-    height: "100vh",
+    flexGrow: 1,
+  },
+  appbar: {
+    backgroundColor: "#00796b",
+  },
+  hero: {
+    backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('https://res.cloudinary.com/khandoors/image/upload/v1590622014/feedback%20form/keee064wcezaqyhmdyb2.jpg')`,
+    height: "60vh",
+    repeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#00796b",
+    fontSize: "4rem",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  container: {
+    paddingTop: theme.spacing(3),
+  },
+  projectTitle: {
+    fontWeight: 800,
+    paddingBottom: theme.spacing(3),
+  },
+  card: {
+    maxWidth: "100%",
+  },
+  media: {
+    height: 240,
+  },
+  cardActions: {
+    display: "flex",
+    margin: "0, 10px",
+    justifyContent: "space-between",
+  },
+  author: {
+    display: "flex",
   },
 });
 
@@ -29,49 +82,85 @@ const Portfolio = () => {
       });
   };
 
-  console.log(items);
-
   return (
-    <div className={classes.root}>
-      <Typography variant="h1" gutterBottom>
-        Portfolios
-      </Typography>
-      <Grid container>
-        <Grid item xs={12}>
-          <Grid
-            container
-            direction="column"
-            justify="center"
-            alignItems="center"
-            spacing={2}
-          >
+    <>
+      <div>
+        <AppBar className={classes.appbar} position="static">
+          <Toolbar variant="dense">
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="primary">
+              Khandoors
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <Box className={classes.hero}>
+        <Box>
+          <h1>The Khandoor</h1>{" "}
+        </Box>
+      </Box>
+      <div className={classes.root}>
+        <Container maxWidth="lg" className={classes.container}>
+          <Typography variant="h4" className={classes.projectTitle}>
+            Projects
+          </Typography>
+          <Grid container spacing={3}>
             {items.map((item) => (
               <Grid item key={item.id}>
-                <Card
-                  style={{
-                    width: "30em",
-                    height: "20em",
-                    backgroundColor: item.background,
-                  }}
-                >
-                  <Typography variant="h3" gutterBottom>
-                    {item.title}
-                  </Typography>
-                  <Typography variant="h5" gutterBottom>
-                    {item.description}
-                  </Typography>
-                  <img
-                    src={item.bgimage}
-                    alt="bgimage"
-                    style={{ height: "20em", width: "20em" }}
-                  />
+                <Card className={classes.card}>
+                  <CardActionArea>
+                    <CardMedia
+                      className={classes.media}
+                      image="https://res.cloudinary.com/khandoors/image/upload/v1590622014/feedback%20form/nldbqltkxqn3mibus1js.png"
+                      title="project title"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {item.title}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="p"
+                      >
+                        {item.description}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions className={classes.cardActions}>
+                    <Box className={classes.author}>
+                      <Avatar src="https://res.cloudinary.com/khandoors/image/upload/v1561893971/feedback%20form/KhandoorMedia.png" />
+                      <Box ml={2}>
+                        <Typography variant="subtitle" component="p">
+                          Khandoor
+                        </Typography>
+                        <Typography
+                          variant="subtitle2"
+                          color="textSecondary"
+                          component="p"
+                        >
+                          {item.created_at}
+                        </Typography>
+                      </Box>
+                    </Box>
+                    <Box>
+                      <BookmarksIcon />
+                    </Box>
+                  </CardActions>
                 </Card>
               </Grid>
             ))}
           </Grid>
-        </Grid>
-      </Grid>
-    </div>
+        </Container>
+      </div>
+    </>
   );
 };
 
